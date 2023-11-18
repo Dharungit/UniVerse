dff312b5186e604948a544b3ab3b9214340fe517
Dharungit <dharun123wizz@gmail.com>
2023-11-18 23:19:10 +0530

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./globals.css";
+import App from "./App";
+
+ReactDom.createRoot(document.getElementById("root")!).render(<App />);
